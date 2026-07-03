65830010e00ea6bdb323f890f3fa52847a9604d5
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-07-03 17:19:38 +0100

    style: fmt

--- src/functions/list_repositories/handler.py
+++ src/functions/list_repositories/handler.py
@@ -4,13 +4,15 @@ from utils.github import get_github_client
 
 from policy_methods_library.utils.pagination import get_paginated_list
 
+
 def handler(event, context):
     """Step Function invokes with {"owner": "..."}."""
     client = get_github_client(event["owner"])
 
-    repositories = get_paginated_list(client, f"/orgs/{event['owner']}/repos?per_page=100", "repositories")
+    repositories = get_paginated_list(
+        client, f"/orgs/{event['owner']}/repos?per_page=100", "repositories"
+    )
 
     print(f"Found {len(repositories)} repositories for owner {event['owner']}")
 
     return repositories
-    
