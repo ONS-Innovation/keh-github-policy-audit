f5dd0e45fb5b2d9f01065bcd4dc9cafb8a1948a4
delterr <Hasan.T.Ahmad@ons.gov.uk>
2026-08-12 12:24:37 +0100

    fix lambda by making api request for branch info

--- github_policy_audit/functions/repository_checks/branch_protection/handler.py
+++ github_policy_audit/functions/repository_checks/branch_protection/handler.py
@@ -14,7 +14,25 @@ logger.setLevel(logging.INFO)
 @github_handler
 def handler(event, context, client):
     """Step Function invokes with {"owner": "...", "repository_name": "...", "branch_name": "..."}."""
-    result = check_branch_protection(client, event["repository_name"], event["branch_name"])
+
+    branches = client.make_request(
+        "GET", f"/repos/{event['owner']}/{event['repository_name']}/branches"
+    ).json()
+
+    branch_name = None
+
+    for branch in branches:
+        if branch["name"] == "master":
+            branch_name = "master"
+            break
+        elif branch["name"] == "main":
+            branch_name = "main"
+            break
+        else:
+            continue
+
+    result = check_branch_protection(client, event["repository_name"], branch_name)
+
     result["check_name"] = "branch_protection"
     log_info(
         logger,
