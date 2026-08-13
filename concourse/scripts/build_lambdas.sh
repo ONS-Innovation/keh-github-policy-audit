9e5724e77a2149656ef4ce643ecb225b092d0473
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-08-13 16:47:49 +0100

    fix(ci): add missing poetry dependency when building lambdas

--- concourse/scripts/build_lambdas.sh
+++ concourse/scripts/build_lambdas.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -eu
 
+# Install Poetry
+curl -sSL https://install.python-poetry.org | python3 -
+
 cd resource-repo
 
+# Build the dependency layer and Lambda functions
 sh ./scripts/build-dependency-layer.sh
 sh ./scripts/build-lambda-functions.sh
