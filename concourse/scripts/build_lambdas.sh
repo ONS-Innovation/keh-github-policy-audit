86cb4521f9d6ada16c173452e4e8b838573847c5
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-08-13 16:23:42 +0100

    temp: pwd

--- concourse/scripts/build_lambdas.sh
+++ concourse/scripts/build_lambdas.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eu
 
+echo "Building Lambda functions and dependency layer..."
+
+echo "Current directory: $(pwd)"
+
 cd resource-repo
 
-sh scripts/build-dependency-layer.sh
-sh scripts/build-lambda-functions.sh
+sh ./scripts/build-dependency-layer.sh
+sh ./scripts/build-lambda-functions.sh
